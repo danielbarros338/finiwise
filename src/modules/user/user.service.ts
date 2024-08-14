175fe28236c771bdf3fbaa072cb2bcdad52cefea
Daniel <usrrufus12@omint.com.br>
2024-08-14 16:16:34 -0300

    Validation of implemented user fields and password encryption

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,11 +4,42 @@ import { HttpStatus } from '@nestjs/common';
 
 import { User } from '../../database/models/user.model';
 
+import { crypt } from '../../utils/crypto';
+
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  constructor(
+    @InjectModel(User) private userRepository: typeof User
+  ) {}
 
   public async signUp(user: User): Promise<Record<string, number>> {
+    if (!user.email || !user.password || !user.user) {
+      throw new HttpException(
+        'Email, password and user is required',
+        HttpStatus.BAD_REQUEST
+      )      
+    }
+
+    try {
+      const userExists = await this.userRepository.findOne({
+        where: { email: user.email } 
+      });
+      
+      if (userExists) {
+        throw new HttpException(
+          'User already exists',
+          HttpStatus.CONFLICT
+        );
+      }
+    } catch (err) {
+      throw new HttpException(
+        'An error occuried while signing up: ' + err.message,
+        err.status
+      );
+    }
+
+    user.password = crypt(user.password);
+
     try {
       const response = await this.userRepository.create<User>(user);
 
