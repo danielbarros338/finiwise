032b5fcd653d3ae332ba455c05ec9e6573ae2a7c
Daniel <danielbarros338@gmail.com>
2024-10-01 22:51:46 -0300

    add route add-balance

--- src/modules/wallet/wallet.controller.ts
+++ src/modules/wallet/wallet.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Logger, Post, HttpException, Req } from '@nestjs/common';
+import { Controller, Logger, Post, HttpException, Req, HttpCode, HttpStatus } from '@nestjs/common';
 import { Request } from 'express';
 
 import { Wallet } from 'src/database/models/wallet.model';
@@ -13,12 +13,25 @@ export class WalletController {
     private readonly walletService: WalletService
   ) {}
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('create')
   async createWallet(@Req() req: Request): Promise<Wallet> {
     try {
       return await this.walletService.createWallet(req.body.userId);
     } catch (err) {
       this.logger.error('createEarning: \n' + err.message);
+
+      throw new HttpException(err.message, err.status);
+    }
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('add-balance')
+  async addBalance(@Req() req: Request): Promise<Wallet> {
+    try {
+      return await this.walletService.addBalance(req.body.userId, req.body.amount);
+    } catch (err) {
+      this.logger.error('addBalance: \n' + err.message);
 
       throw new HttpException(err.message, err.status);
     }
